3e813b8abcc6a60b97cbafc9ba5ff7a228b6196c
taaaf11 <taafuuu@gmail.com>
2025-02-20 09:10:04 +0500

    Fix implementaiton of methods (`Row`)

--- src/matop/row.py
+++ src/matop/row.py
@@ -1,9 +1,5 @@
 from __future__ import annotations
 
-# from typing import override
-# from numbers import int
-# from row import Column
-
 
 class Row:
     def __init__(self, *nums: int):
@@ -27,18 +23,16 @@ class Row:
         return product
     
     def mul_by_scalar(self, scalar: int):
-        new_row: list[int] = []
-        
-        for num in self.nums:
-            new_row.append(num * scalar)
+        for index in range(len(self.__nums)):
+            self.__nums[index] *= scalar
         
     def as_latex(self) -> str:
-        return " & ".join(map(str, self.nums))
+        return " & ".join(map(str, self.__nums))
     
     def __add__(self, other: Row) -> Row:
         new_row: list[int] = []
         
-        for (index, num) in enumerate(self.nums):
+        for (index, num) in enumerate(self.__nums):
             new_row.append(num + other.nums[index])
             
         return Row(*new_row)
@@ -46,20 +40,19 @@ class Row:
     def __eq__(self, value: object) -> bool:
         if not isinstance(value, Row):
             return NotImplemented
-        return self.nums == value.nums
+        return self.__nums == value.nums
     
     def __len__(self):
-        return len(self.nums)
+        return len(self.__nums)
     
     def __iter__(self):
-        for num in self.nums:
+        for num in self.__nums:
             yield num
     
     def __repr__(self) -> str:
-        return "Row(" + ", ".join(map(str, self.nums)) + ")"
-    
-    
-    
+        return "Row(" + ", ".join(map(str, self.__nums)) + ")"
+
+ 
 if __name__ == "__main__":
     row = Row(1,2,3)
     print(row.as_latex())
